2cece13dc46efc6ec8192826be6fce297a34189b
Alex Herasymenko <alex.herasymenko@cambridgebrainsciences.com>
2024-03-11 19:24:00 +0000

    fixed typo build script as part of the build process

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ echo "Test step - Run fake integration tests for Android"
 echo "flutter test integration_test --passed"
 
 # Build Part 
-echo "Build step - Build app for Android
+echo "Build step - Build app for Android"
 flutter build apk --release
