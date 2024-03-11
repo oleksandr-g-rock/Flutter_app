1c3308565dbd932f0819ecec5c8cd0a1deb2db0e
Alex Herasymenko <alex.herasymenko@cambridgebrainsciences.com>
2024-03-11 19:21:32 +0000

    Modify your GitHub action pipeline to run your build script as part of the build process.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Test part
+echo "Test step - Install dependencies"
+flutter pub get
+echo "Test step - Run analyze"
+flutter analyze
+echo "Test step - Run flutter tests"
+flutter test
+echo "Test step - Run fake integration tests for Android"
+echo "flutter test integration_test --passed"
+
+# Build Part 
+echo "Build step - Build app for Android
+flutter build apk --release
